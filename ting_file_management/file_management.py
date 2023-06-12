e2aeaa037d13f449d3f03a9a8a4694ae65adb0ca
Layane Nunes <layanenu@gmail.com>
2023-06-12 11:09:56 -0300

    feat: function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            sys.stderr.write('Formato inválido\n')
+
+        with open(path_file) as archive:
+            archive_content = archive.read().split("\n")
+            return archive_content
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
