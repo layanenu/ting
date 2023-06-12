6b1830ac3d2e277c65afdafc0f150d4a3c6e2b3d
Layane Nunes <layanenu@gmail.com>
2023-06-12 15:18:04 -0300

    feat: function exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_results = []
+
+    queue_list = list(instance.queue)
+
+    for index_one, content in enumerate(queue_list):
+        line_list = content["linhas_do_arquivo"]
+
+        occurrences = []
+        for index_two, line in enumerate(line_list):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index_two + 1})
+
+        if occurrences:
+            search_results.append({
+                "palavra": word,
+                "arquivo": content["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            })
+
+    return search_results
 
 
 def search_by_word(word, instance):
